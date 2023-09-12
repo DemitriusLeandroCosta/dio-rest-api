f176ca78db3266f38eedde9077fdc7cdeb0d4792
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2023-09-12 17:09:17 -0300

    first commit

---
+++ src/main/java/estudo/aplicationapi/AplicationApiApplication.java
@@ -0,0 +1,13 @@
+package estudo.aplicationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudo/aplicationapi/AplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.aplicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
